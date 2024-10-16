5a9b4fd590eac688c8ce2528b0ad29ac727f4690
AndrzejBudzyn <andrzej.budzyn@studenci.collegiumwitelona.pl>
2024-10-16 16:13:10 +0200

    Update 0001_01_01_000000_create_users_table.php

--- Backend/database/migrations/0001_01_01_000000_create_users_table.php
+++ Backend/database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,7 +15,6 @@ return new class() extends Migration {
             $table->string("email")->unique();
             $table->timestamp("email_verified_at")->nullable();
             $table->string("password");
-            $table->string("role")->default("user");
             $table->json("favorites")->nullable();
             $table->rememberToken();
             $table->timestamps();
