b3f678905a3bfadf9843a4e423dde653fecd9063
AndrzejBudzyn <115713433+AndrzejBudzyn@users.noreply.github.com>
2024-10-16 17:05:15 +0200

    Database and migration complete for now (#23)
    * Database and migration complete for now
    
    * Update 0001_01_01_000000_create_users_table.php
    
    * Update 2024_10_16_105416_create_kebabs_table.php

---
+++ Backend/database/migrations/2024_10_16_105416_create_kebabs_table.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration {
+    public function up(): void
+    {
+        Schema::create("kebabs", function (Blueprint $table): void {
+            $table->id();
+            $table->binary('logo')->nullable(); 
+            $table->string("name");
+            $table->string("address"); 
+            $table->json("coordinates"); 
+            $table->json("sauces"); 
+            $table->json("meats"); 
+            $table->enum("status", ["exists", "closed", "planned"]); 
+            $table->json("opening_hours"); 
+            $table->year("opening_year")->nullable(); 
+            $table->year("closing_year")->nullable(); 
+            $table->boolean("is_crafted"); 
+            $table->boolean("is_premises"); 
+            $table->boolean("is_chainstore"); 
+            $table->json("ordering_options"); 
+            $table->json("comments")->nullable(); 
+            $table->float("google_reviews", 2, 1)->nullable(); 
+            $table->float("pysznepl_reviews", 2, 1)->nullable(); 
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists("kebabs");
+    }
+};
