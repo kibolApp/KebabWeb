6e03f9269b1b55524668c2c1579fe0318278aae5
AndrzejBudzyn <115713433+AndrzejBudzyn@users.noreply.github.com>
2024-10-19 00:57:40 +0200

    Hosting and fix with database (#26)
    * Hosting and fix with database
    
    * Update 2024_10_16_105416_create_kebabs_table.php

--- Backend/database/migrations/2024_10_16_105416_create_kebabs_table.php
+++ Backend/database/migrations/2024_10_16_105416_create_kebabs_table.php
@@ -11,7 +11,7 @@ return new class() extends Migration {
     {
         Schema::create("kebabs", function (Blueprint $table): void {
             $table->id();
-            $table->binary('logo')->nullable(); 
+            $table->binary("logo")->nullable(); 
             $table->string("name");
             $table->string("address"); 
             $table->json("coordinates"); 
