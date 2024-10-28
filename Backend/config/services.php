9a315ff814e9737f077020d08fcc6fdf50a5cd4a
AndrzejBudzyn <andrzej.budzyn@studenci.collegiumwitelona.pl>
2024-10-28 13:40:01 +0100

    adjustments

--- Backend/config/services.php
+++ Backend/config/services.php
@@ -15,27 +15,8 @@ return [
     |
     */
 
-    "mailgun" => [
-        "domain" => env("MAILGUN_DOMAIN"),
-        "secret" => env("MAILGUN_SECRET"),
-        "endpoint" => env("MAILGUN_ENDPOINT", "api.mailgun.net"),
-        "scheme" => "https",
-    ],
-
     "postmark" => [
         "token" => env("POSTMARK_TOKEN"),
-    ],
-
-    "github" => [
-        "client_id" => env("GITHUB_CLIENT_ID"),
-        "client_secret" => env("GITHUB_CLIENT_SECRET"),
-        "redirect" => "http://127.0.0.1:8000/auth/github/callback",
-    ],
-
-    "google" => [
-        "client_id" => env("GOOGLE_CLIENT_ID"),
-        "client_secret" => env("GOOGLE_CLIENT_SECRET"),
-        "redirect" => "http://127.0.0.1:8000/auth/google/callback",
     ],
 
     "ses" => [
