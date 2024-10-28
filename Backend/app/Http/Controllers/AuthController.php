19d2df81247b89e6f29455671af9a114132a7c00
AndrzejBudzyn <115713433+AndrzejBudzyn@users.noreply.github.com>
2024-10-28 13:46:48 +0100

    laravel fix and auth working (#28)
    * laravel fix and auth working
    
    * clean code
    
    * .
    
    * adjustments

---
+++ Backend/app/Http/Controllers/AuthController.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\SignupRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(SignupRequest $request)
+    {
+        $data = $request->validated();
+        /** @var User $user */
+        $user = User::create([
+            "name" => $data["name"],
+            "email" => $data["email"],
+            "password" => bcrypt($data["password"]),
+            "isAdmin" => false,
+        ]);
+        $token = $user->createToken((int)["id" => (string)$user->id])->plainTextToken;
+
+        $res = [
+            "user" => $user,
+            "token" => $token,
+        ];
+
+        return response($res);
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->validated();
+
+        if (!Auth::attempt($credentials)) {
+            return response([
+                "message" => "Something wrong",
+            ]);
+        }
+        /** @var User $user */
+        $user = Auth::user();
+        $token = $user->createToken((int)["id" => (string)$user->id])->plainTextToken;
+        $res = [
+            "user" => $user,
+            "token" => $token,
+        ];
+
+        return response($res);
+    }
+
+    public function logout(Request $request)
+    {
+        /** @var User $user */
+        $user = $request->user();
+        $user->currentAccessToken()->delete;
+
+        return response("", 204);
+    }
+}
--- Backend/database/factories/UserFactory.php
+++ Backend/database/factories/UserFactory.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 /**
@@ -24,7 +23,7 @@ class UserFactory extends Factory
             "name" => fake()->name(),
             "email" => fake()->unique()->safeEmail(),
             "email_verified_at" => now(),
-            "password" => static::$password ??= Hash::make("password"),
+            "password" => "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi", // password
             "remember_token" => Str::random(10),
         ];
     }
@@ -38,7 +37,4 @@ class UserFactory extends Factory
             "email_verified_at" => null,
         ]);
     }
-
-    /** The current password being used by the factory. */
-    protected static ?string $password;
 }
