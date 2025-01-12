9be96254b90a5f4057d5620bc5e6a933b4e67237
KacperWojdak <106652257+KacperWojdak@users.noreply.github.com>
2025-01-12 13:05:39 +0100

    32 frontend tests (#33)
    * Some of the e2e tests
    
    * Added delay
    
    * Revert "Added delay"
    
    This reverts commit 0ac3967daae598a3337e6b6e4d0a974e12f54491.
    
    * added delay after revert
    
    * fixing eslint problems with cypress
    
    * back to c.wait
    
    ---------
    
    Co-authored-by: PiotrFedak <piotr.fedak@studenci.collegiumwitelona.pl>

---
+++ frontend/cypress/e2e/RegistrationError.cy.js
@@ -0,0 +1,20 @@
+/* eslint-env cypress */
+
+describe('User Registration with Existing Email', () => {
+    it('Shows an error when trying to register with an existing email', () => {
+      cy.visit('http://localhost:3000/auth');
+  
+      cy.contains('Zarejestruj się.').click();
+  
+      cy.get('input[placeholder="Nazwa użytkownika"]').type('AdminUser');
+      cy.get('input[placeholder="Adres E-mail"]').type('admin@admin.pl');
+      cy.get('input[placeholder="Hasło"]').type('password123');
+      cy.get('input[placeholder="Potwierdź hasło"]').type('password123');
+  
+      cy.get('button[type="submit"]').click();
+  
+      cy.contains('Błąd serwera: The email has already been taken.').should('be.visible');
+      cy.url().should('include', '/auth');
+    });
+  });
+  
