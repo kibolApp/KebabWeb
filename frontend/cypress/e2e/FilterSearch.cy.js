6757acf05d0344e1361dd715a1d21316e33fbb27
KacperWojdak <kacper.wojdak@interia.pl>
2025-01-12 13:03:22 +0100

    back to c.wait

--- frontend/cypress/e2e/FilterSearch.cy.js
+++ frontend/cypress/e2e/FilterSearch.cy.js
@@ -6,8 +6,7 @@ describe('Filter and search for a kebab', () => {
       cy.get('input[placeholder="Adres E-mail"]').type('admin@admin.pl');
       cy.get('input[placeholder="Hasło"]').type('admin');
       cy.get('button[type="submit"]').click();
-      cy.intercept('GET', '/api/kebabs').as('fetchKebabs');
-      cy.wait('@fetchKebabs');
+      cy.wait(2000);
   
       cy.url().should('include', '/map');
   
